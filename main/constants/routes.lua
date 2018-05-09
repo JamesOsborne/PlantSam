15254757db1ef598c8b49de59b5e573fd6480e29
JamesOsborne <james.osborne112@gmail.com>
2018-05-09 13:40:44 -0500

    Switch out hello with crappy plant view pic

---
+++ main/constants/routes.lua
@@ -0,0 +1,6 @@
+COLLECTION_PROXY_SWITCHER = 'loader#collection_proxy_switcher'
+PLANT_VIEWER_PROXY = 'loader#plant_viewer'
+MAIN_MENU = {
+	GAME_OBJECT = 'main_menu',
+	GUI = 'main_menu#main_menu'
+}
