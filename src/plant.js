02344226d9d7bc573cade8daf25dc6f879eaf58b
Kyle Perik <kyle@periks.net>
2018-05-10 22:53:59 -0500

    curvy green plant, and improvements

--- src/plant.js
+++ src/plant.js
@@ -1,13 +1,27 @@
 class Plant {
-    constructor(pos = createVector(0, 0), branches = []) {
+    constructor(pos = createVector(500, 800), branches = []) {
         this.pos = pos;
         this.branches = branches;
     }
 
     draw() {
-        console.log(this.branches);
-        for (let branch of this.branches) {
-            branch.draw();
+        strokeWeight(20);
+        noFill();
+        stroke(100, 180, 0);
+        beginShape();
+        // start ancor and start point
+        curveVertex(this.pos.x, this.pos.y);
+        curveVertex(this.pos.x, this.pos.y);
+        this.branches.forEach((branch, i) => {
+            curveVertex(branch.begin.x, branch.begin.y);
+        });
+        // end ancor
+        if (this.branches.length) {
+            var begin = this.branches[this.branches.length - 1].begin;
+            curveVertex(
+                begin.x, begin.y
+            );
         }
+        endShape();
     }
 }
