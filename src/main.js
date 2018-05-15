57f117ee97496355e5358148f3149351a6862461
JamesOsborne <james.osborne112@gmail.com>
2018-05-15 16:43:55 -0500

    Change myBranch to currBranch and use lodash functions

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 let plant;
-let mybranch;
+let currBranch;
 let particleEmitters;
 
 function setup() {
@@ -8,7 +8,7 @@ function setup() {
 
     plant = new Plant();
     begin = plant.pos;
-    mybranch = plant;
+    currBranch = plant;
 
     particleEmitters = [];
     frameRate(2);
@@ -26,31 +26,43 @@ function draw() {
 
 function mouseClicked() {
     let pos = createVector(mouseX, mouseY);
+
     if (mouseButton === LEFT) {
-        var points = plant.branches.reduce(
-            (r, x) => r.concat(V.add(
-                (r[r.length - 1] || plant.pos),
-                V.mult(new V(Math.cos(x.angle), Math.sin(x.angle)), x.length)
-            )),
+        let points = _.reduce(
+            plant.branches,
+            (r, x) => _.concat(
+                r,
+                Vector.add(
+                    (r[r.length - 1] || plant.pos),
+                    Vector.mult(
+                        new Vector(Math.cos(x.angle), Math.sin(x.angle)),
+                        x.length)
+                )
+            ),
             []
         );
 
-        var last = (points[points.length - 1] || plant.pos);
-        var structure = getStructure(plant.pos, 0, plant);
-        var mybranchStructure = getBranchStructure(structure, mybranch);
-        relative = V.sub(pos, mybranchStructure.pos);
+        let last = (points[points.length - 1] || plant.pos);
+        let structure = getStructure(plant.pos, 0, plant);
+        let branchStructure = getBranchStructure(structure, currBranch);
+        let relative = Vector.sub(pos, branchStructure.pos);
 
-        mybranch.branches = [new Branch(
-            Math.atan2(relative.y, relative.x) - mybranchStructure.angle,
-            Math.sqrt(Math.pow(relative.x, 2) + Math.pow(relative.y, 2)),
-            5,
-            [1, 2].map(i => new Branch(
-                random(-Math.PI/2, Math.PI/2),
-                20,
+        currBranch.branches = _.concat([
+            new Branch(
+                Math.atan2(relative.y, relative.x) - branchStructure.angle,
+                Math.sqrt(Math.pow(relative.x, 2) + Math.pow(relative.y, 2)),
                 5,
-                [new Branch(0.01, 20, 2, [])]
-            ))
-        )].concat(mybranch.branches).sort((a, b) => a.angle - b.angle);
-        mybranch = mybranch.branches.filter(b => b.branches.length > 1)[0];
+                _.map([1, 2], num =>
+                    new Branch(
+                        random(-HALF_PI, HALF_PI),
+                        20,
+                        5,
+                        [new Branch(0.01, 20, 2, [])]
+                    )
+                )
+            )],
+            _.sortBy(currBranch.branches, branch => branch.angle)
+        );
+        currBranch = _.filter(currBranch.branches, branch => branch.branches.length > 1)[0];
     }
 }
